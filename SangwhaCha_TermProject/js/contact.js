c11d90f8a8aff078db68f196d5ef04e4dcd1ba43
Sangwha Cha <44427215+ssanghwacha@users.noreply.github.com>
2024-12-13 10:37:34 -0800

    Add files via upload

--- SangwhaCha_TermProject/js/contact.js
+++ SangwhaCha_TermProject/js/contact.js
@@ -1,3 +1,28 @@
+// Handle hamburger menu toggle
+const hamburger = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+let isOpen = false;
+
+hamburger.addEventListener('click', () => {
+    const spans = document.querySelectorAll('.hamburger-icon span');
+
+    // Animate hamburger icon and show/hide menu
+    if (isOpen) {
+        spans[0].style.transform = 'translateY(0) rotate(0)';
+        spans[1].style.opacity = '1';
+        spans[2].style.transform = 'translateY(0) rotate(0)';
+        mobileMenu.classList.remove('show');
+    } else {
+        spans[0].style.transform = 'translateY(12px) rotate(-135deg)';
+        spans[1].style.opacity = '0';
+        spans[2].style.transform = 'translateY(-12px) rotate(135deg)';
+        mobileMenu.classList.add('show');
+    }
+
+    isOpen = !isOpen; // Toggle menu state
+    console.log('open:', isOpen);
+});
 $(document).ready(function () {
     // Initialize EmailJS with your public key
     emailjs.init('GCRK4etZDLbMRsn-N'); // Public Key를 입력
